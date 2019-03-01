0b04239d9709eaa72bc1c63b8000143a0587a452
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-01 14:07:00 +0500

    .

---
+++ load.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2019
+ * 
+ * 
+ * Application Loader
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Savera
+ */
+
+  // Define ABSPATH as this file's directory
+  if (!defined('ABSPATH'))
+  define('ABSPATH', dirname(__FILE__).'/');
