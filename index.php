6c912e01ee7713fa5f1e38e54c2960e5bc01a22f
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-01 15:03:42 +0500

    .

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2019
+ * 
+ * 
+ * Application Loader
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Savera
+ */
+  
+  require('load.php');
