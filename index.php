ea3c0b4a6d898cbe1bfa1192394c9e66ab4bb596
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-01 15:32:27 +0500

    .

--- index.php
+++ index.php
@@ -17,16 +17,26 @@
   
   require('load.php');
 
+  $query_response = '';
+
   if(isset($_POST['query'])) {
     $query = mysqli_real_escape_string($link, $_POST['sql_statement']);
-    mysqli_query($link, "");
+    $result = mysqli_query($link, $query);
+    if($result) {
+      $query_response = 'Query OK';
+    }else {
+      $query_response = 'Query Failed';
+    }
   }
 
 ?>
 <center>
   <form action="" method='post'>
     <textarea type="textarea" placeholder='Enter SQL Statment here...' rows='5' cols='100' name='sql_statement'></textarea>
-    <br><br>
+    <br>
+    <?php echo $query_response; ?>
+    <br>
     <input type="submit" name='query' value='Query'>
   </form>
 </center>
+
