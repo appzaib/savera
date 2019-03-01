92df6716ea99bbd5ad3d87aaf6222b3228b8e1fd
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-03-01 14:20:27 +0500

    .

---
+++ config-sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Appzaib (https://appzaib.com)
+ * @copyright Appzaib 2019
+ * 
+ * 
+ * Application Configuration
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Savera
+ */
+
+  // Application's database server credentials
+  define("HOST", "");
+  define("DB", "");
+  define("USER", "");
+  define("PASS", "");
